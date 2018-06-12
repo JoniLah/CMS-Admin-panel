cb33fb3c62475d95d356d1b83fd89e4d10c4e172
JoniLah <joni.lahdesmaki@hotmail.fi>
2018-06-12 17:27:37 +0300

    Change from input field to dropdown

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -96,8 +96,16 @@
     </div>
 
     <div class="form-group">
-        <label for="post_status">Post Status</label>
-        <input type="text" value="<?php echo $post_status; ?>" class="form-control" name="post_status">
+        <select name="post_status" id="">
+            <option value="<?php echo $post_status; ?>"><?php echo $post_status; ?></option>
+            <?php
+                if($post_status == "published") {
+                    echo "<option value='draft'>draft</option>";
+                } else {
+                    echo "<option value='published'>published</option>";
+                }
+            ?>
+        </select>
     </div>
 
     <div class="form-group">
