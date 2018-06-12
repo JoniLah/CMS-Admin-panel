df18848744df166708c149e55522cea878e1885d
JoniLah <joni.lahdesmaki@hotmail.fi>
2018-06-12 19:35:43 +0300

    Add checkbox for each post to modify them quickly

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -5,5 +5,17 @@ $(document).ready(function() {
         console.error(error);
     });
 
+    $("#selectAllBoxes").click(function(event){
+        if (this.checked) {
+            $(".checkBoxes").each(function() {
+                this.checked = true;
+            });
+        } else {
+            $(".checkBoxes").each(function() {
+                this.checked = false;
+            });
+        }
+    });
+
 });
 
