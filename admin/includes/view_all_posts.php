3437fe7fb909026430eb8adff806d3dbf03f1476
JoniLah <joni.lahdesmaki@hotmail.fi>
2018-06-13 19:25:10 +0300

    View count for posts

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -69,6 +69,7 @@
                 <th>Tags</th>
                 <th>Comments</th>
                 <th>Date</th>
+                <th>Views</th>
                 <th>View Post</th>
                 <th>Edit</th>
                 <th>Delete</th>
@@ -89,6 +90,7 @@
                     $post_tags = $row['post_tags'];
                     $post_comment_count= $row['post_comment_count'];
                     $post_date = $row['post_date'];
+                    $post_views_count = $row['post_views_count'];
                     
                     echo "<tr>";
                     ?>
@@ -113,6 +115,7 @@
                     echo "<td>{$post_tags}</td>";
                     echo "<td>{$post_comment_count}</td>";
                     echo "<td>{$post_date}</td>";
+                    echo "<td>{$post_views_count}</td>";
                     echo "<td><a href='../post.php?p_id=$post_id'>View Post</a></td>";
                     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                     echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete this item?');\" href='posts.php?delete={$post_id}'>Delete</a></td>";
