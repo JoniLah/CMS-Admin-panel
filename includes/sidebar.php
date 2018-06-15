e999c9cb8824ee3b6376ac74a76db275ae51c902
JoniLah <joni.lahdesmaki@hotmail.fi>
2018-06-15 06:31:21 +0300

    Disable login form if logged in and display logout button

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -17,20 +17,29 @@
 
     <!-- Login Well -->
     <div class="well">
-        <h4>Login</h4>
-        <form action="includes/login.php" method="post">
-            <div class="form-group">
-                <input name="username" type="text" class="form-control" placeholder="Username">
-            </div>
-            <div class="input-group">
-                <input name="password" type="password" class="form-control" placeholder="Password">
-                <span class="input-group-btn">
-                    <button class="btn btn-primary" name="login" type="submit">Submit
-                    </button>
-                </span>
-            </div>
-        </form> <!-- search form-->
-        <!-- /.input-group -->
+        <?php
+            // Shorthand method for if statement
+            if(isset($_SESSION['role'])):
+        ?>
+            <h4>Logged in as <?php echo $_SESSION['username']; ?></h4>
+            <a href="includes/logout.php" class="btn btn-primary">Logout</a>
+
+        <?php else: ?>
+            <h4>Login</h4>
+            <form action="includes/login.php" method="post">
+                <div class="form-group">
+                    <input name="username" type="text" class="form-control" placeholder="Username">
+                </div>
+                <div class="input-group">
+                    <input name="password" type="password" class="form-control" placeholder="Password">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" name="login" type="submit">Submit
+                        </button>
+                    </span>
+                </div>
+            </form> <!-- search form-->
+            <!-- /.input-group -->
+        <?php endif; ?>
     </div>
 
     <!-- Blog Categories Well -->
