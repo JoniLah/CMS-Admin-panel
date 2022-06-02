8c8d9d6ab9f798472b8706df2a8a788cf524a616
Joni <joni.lahdesmaki@hotmail.fi>
2022-06-02 06:26:49 +0300

    Update config.php

--- classes/config.php
+++ classes/config.php
@@ -1,15 +1,10 @@
 <?php
 
 class Config {
-    /*
-    const SMTP_HOST = "smtp.mailtrap.io";
-    const SMTP_PORT = "2525";
-    const SMTP_USER = "8760041950ea94";
-    const SMTP_PASSWORD = "84b91ca35f10c3";*/
     const SMTP_HOST = "localhost";
     const SMTP_PORT = "587";
-    const SMTP_USER = "support@feleroid.com";
-    const SMTP_PASSWORD = "supportfeleroid";
+    const SMTP_USER = "your@email.com";
+    const SMTP_PASSWORD = "yourpassword";
 }
 
 ?>
